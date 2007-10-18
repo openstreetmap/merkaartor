5a54227a6fbda6a5573843619d1aab0ac4c30a97
Bart Vanhauwaert <bvh-osm@irule.be>
2007-10-18 11:06:54 +0000

    FIX : undo/redo handling when manipulating tags
    git-svn-id: http://svn.openstreetmap.org/applications/editors/merkaartor@5062 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- Command/FeatureCommands.cpp
+++ Command/FeatureCommands.cpp
@@ -3,102 +3,69 @@
 #include "Map/MapFeature.h"
 #include "Sync/DirtyList.h"
 
-SetTagCommand::SetTagCommand(MapFeature* aF, unsigned int idx, const QString& k, const QString& v)
-: theFeature(aF), Idx(idx), Key(k), Value(v), OldKey(aF->tagKey(idx)), OldValue(aF->tagValue(idx)), Remove(false)
+TagCommand::TagCommand(MapFeature* aF)
+: theFeature(aF), FirstRun(true)
 {
+	for (unsigned int i=0; i<theFeature->tagSize(); ++i)
+		Before.push_back(std::make_pair(theFeature->tagKey(i),theFeature->tagValue(i)));
+}
+
+void TagCommand::undo()
+{
+	theFeature->clearTags();
+	for (unsigned int i=0; i<Before.size(); ++i)
+		theFeature->setTag(Before[i].first,Before[i].second);
+}
+
+void TagCommand::redo()
+{
+	if (FirstRun)
+		for (unsigned int i=0; i<theFeature->tagSize(); ++i)
+			After.push_back(std::make_pair(theFeature->tagKey(i),theFeature->tagValue(i)));
+	else
+	{
+		theFeature->clearTags();
+		for (unsigned int i=0; i<After.size(); ++i)
+			theFeature->setTag(After[i].first,After[i].second);
+	}
+	FirstRun = false;
+}
+
+bool TagCommand::buildDirtyList(DirtyList& theList)
+{
+	if (Before == After) return true;
+	return theList.update(theFeature);
+}
+
+SetTagCommand::SetTagCommand(MapFeature* aF, unsigned int idx, const QString& k, const QString& v)
+: TagCommand(aF)
+{
+	aF->setTag(idx,k,v);
 	redo();
 }
 
 SetTagCommand::SetTagCommand(MapFeature* aF, const QString& k, const QString& v)
-: theFeature(aF), Idx(aF->findKey(k)), Key(k), Value(v), Remove(true)
+: TagCommand(aF)
 {
-	if (Idx < theFeature->tagSize())
-	{
-		OldKey = theFeature->tagKey(Idx);
-		OldValue = theFeature->tagValue(Idx);
-		Remove = false;
-	}
+	aF->setTag(k,v);
 	redo();
-}
-
-
-void SetTagCommand::redo()
-{
-	if (Idx < theFeature->tagSize())
-		theFeature->setTag(Idx,Key,Value);
-	else
-		theFeature->setTag(Key,Value);
-}
-
-void SetTagCommand::undo()
-{
-	if (!Remove)
-		theFeature->setTag(Idx,OldKey, OldValue);
-	else
-		theFeature->removeTag(Idx);
-}
-
-bool SetTagCommand::buildDirtyList(DirtyList& theList)
-{
-	return theList.update(theFeature);
 }
 
 /* CLEARTAGSCOMMAND */
 
 ClearTagsCommand::ClearTagsCommand(MapFeature* F)
-: theFeature(F)
+: TagCommand(F)
 {
-	for (unsigned int i=0; i<theFeature->tagSize(); ++i)
-		Tags.push_back(std::make_pair(theFeature->tagKey(i),theFeature->tagValue(i)));
+	F->clearTags();
 	redo();
 }
 
-void ClearTagsCommand::redo()
-{
-	theFeature->clearTags();
-}
-
-void ClearTagsCommand::undo()
-{
-	for (unsigned int i=0; i<Tags.size(); ++i)
-		theFeature->setTag(Tags[i].first,Tags[i].second);
-}
-
-bool ClearTagsCommand::buildDirtyList(DirtyList& theList)
-{
-	return theList.update(theFeature);
-}
 
 /* CLEARTAGCOMMAND */
 
 ClearTagCommand::ClearTagCommand(MapFeature* F, const QString& k)
-: theFeature(F), Existed(false), Key(k)
+: TagCommand(F)
 {
-	unsigned int Idx = theFeature->findKey(k);
-	if (Idx < theFeature->tagSize())
-	{
-		Existed = true;
-		Value = theFeature->tagValue(Idx);
-	}
+	F->clearTag(k);
 	redo();
 }
-
-void ClearTagCommand::undo()
-{
-	if (Existed)
-		theFeature->setTag(Key,Value);
-}
-
-void ClearTagCommand::redo()
-{
-	theFeature->clearTag(Key);
-}
-
-bool ClearTagCommand::buildDirtyList(DirtyList& theList)
-{
-	if (!Existed)
-		return true;
-	return theList.update(theFeature);
-}
-
-
--- Command/FeatureCommands.h
+++ Command/FeatureCommands.h
@@ -10,48 +10,38 @@
 
 class MapFeature;
 
-class SetTagCommand :	public Command
+class TagCommand : public Command
+{
+	public:
+		TagCommand(MapFeature* theFeature);
+
+		virtual void undo();
+		virtual void redo();
+		virtual bool buildDirtyList(DirtyList& theList);
+
+	private:
+		MapFeature* theFeature;
+		std::vector<std::pair<QString, QString> > Before, After;
+		bool FirstRun;
+};
+
+class SetTagCommand : public TagCommand
 {
 	public:
 		SetTagCommand(MapFeature* aF, unsigned int idx, const QString& k, const QString& v);
 		SetTagCommand(MapFeature* aF, const QString& k, const QString& v);
-
-		virtual void undo();
-		virtual void redo();
-		virtual bool buildDirtyList(DirtyList& theList);
-
-	private:
-		MapFeature* theFeature;
-		unsigned int Idx;
-		QString Key, Value, OldKey, OldValue;
-		bool Remove;
 };
 
-class ClearTagsCommand : public Command
+class ClearTagsCommand : public TagCommand
 {
 	public:
 		ClearTagsCommand(MapFeature* aF);
-
-		virtual void undo();
-		virtual void redo();
-		virtual bool buildDirtyList(DirtyList& theList);
-	private:
-		MapFeature* theFeature;
-		std::vector<std::pair<QString, QString> > Tags;
 };
 
-class ClearTagCommand : public Command
+class ClearTagCommand : public TagCommand
 {
 	public:
 		ClearTagCommand(MapFeature* aF, const QString& k);
-
-		virtual void undo();
-		virtual void redo();
-		virtual bool buildDirtyList(DirtyList& theList);
-	private:
-		MapFeature* theFeature;
-		bool Existed;
-		QString Key,Value;
 };
 
 #endif
