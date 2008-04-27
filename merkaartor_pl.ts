8fdb4b888f85eff0e9a86de33422d6ed31449074
devrise <devrise@dummy.com>
2008-04-27 18:54:04 +0000

    Added initial polish translation.
    git-svn-id: http://svn.openstreetmap.org/applications/editors/merkaartor@7560 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ merkaartor_pl.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="pl">
+<defaultcodec></defaultcodec>
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="MainWindow.cpp" line="194"/>
+        <source>Supported formats (*.mdc *.gpx *.osm *.ngt *.nmea *.nme)
+Merkaartor document (*.mdc)
+GPS Exchange format (*.gpx)
+OpenStreetMap format (*.osm)
+Noni GPSPlot format (*.ngt)
+NMEA GPS log format (*.nmea *.nme)
+All Files (*)</source>
+        <translation type="unfinished">Obsługiwane formaty (*.mdc *.gpx *.osm *.ngt *.nmea *.nme)
+Merkaartor dokument (*.mdc)
+GPS Exchange format (*.gpx)
+OpenStreetMap format (*.osm)
+Noni GPSPlot format (*.ngt)
+NMEA GPS log format (*.nmea *.nme)
+Wszystkie pliki (*)</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="202"/>
+        <source>Supported formats (*.gpx *.osm *.ngt *.nmea *.nme)
+GPS Exchange format (*.gpx)
+OpenStreetMap format (*.osm)
+Noni GPSPlot format (*.ngt)
+NMEA GPS log format (*.nmea *.nme)
+All Files (*)</source>
+        <translation type="unfinished">Obsługiwane formaty (*.gpx *.osm *.ngt *.nmea *.nme)￼￼
+GPS Exchange format (*.gpx)
+OpenStreetMap format (*.osm)
+Noni GPSPlot format (*.ngt)
+NMEA GPS log format (*.nmea *.nme)
+Wszystkie pliki (*)</translation>
+    </message>
+</context>
+</TS>
