d31c7558b0689ee97a63f217e174f22cab8ff0fa
Dirk Stöcker <openstreetmap@dstoecker.de>
2008-05-02 22:26:53 +0000

    style cleanup
    git-svn-id: http://svn.openstreetmap.org/applications/editors/merkaartor@7630 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- Map/PreDefinedTags.cpp
+++ Map/PreDefinedTags.cpp
@@ -57,7 +57,7 @@ void fillHighway(QComboBox* Box)
 	Box->addItem(QCoreApplication::translate("Highway","Residential road"), "residential");
 	Box->addItem(QCoreApplication::translate("Highway","Pedestrian priority road"), "living_street");
 	Box->addItem(QCoreApplication::translate("Highway","Service road"), "service");
-	Box->addItem(QCoreApplication::translate("Highway","Unsurfaced road"), "unsurfaced");
+	Box->addItem(QCoreApplication::translate("Highway","Unsurfaced road (old tag)"), "unsurfaced");
 	Box->addItem(QCoreApplication::translate("Highway","Track road"), "track");
 	Box->addItem(QCoreApplication::translate("Highway","Cycleway"), "cycleway");
 	Box->addItem(QCoreApplication::translate("Highway","Footway"), "footway");
