e2ac4c43e6e9a6aa5d39a9503786a92b0e0f103d
Dirk Stöcker <openstreetmap@dstoecker.de>
2009-02-16 16:35:15 +0000

    add c-format to .po files, allows more sanity checks
    git-svn-id: http://svn.openstreetmap.org/applications/editors/merkaartor@13760 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- translations/i18n.pl
+++ translations/i18n.pl
@@ -353,6 +353,7 @@ sub createpos($$@)
         next;
         # print FILE "#: unknown:0\n"
       }
+      print FILE "#, c-format\n" if $ennc =~ /\%[0-9n]/;
       print FILE "msgctxt \"$ctx\"\n" if $ctx;
       print FILE "msgid \"$ennc\"\n";
       print FILE "msgid_plural \"$data->{$en}{\"en.1\"}\"\n" if $data->{$en}{"en.1"};
