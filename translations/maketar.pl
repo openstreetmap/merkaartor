c5f99348575ab5e0756d94467fe3436f109c6e88
Dirk Stöcker <openstreetmap@dstoecker.de>
2009-03-04 10:39:00 +0000

    minor fix in upload script
    git-svn-id: http://svn.openstreetmap.org/applications/editors/merkaartor@13967 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- translations/maketar.pl
+++ translations/maketar.pl
@@ -11,6 +11,7 @@ foreach my $f (@ARGV)
   if($f =~ /\*/) { printf "Skipping $f\n"; }
   elsif($f =~ /${templ}_(.*)\.po$/) { $files{$1}{$2} = $f; }
   elsif($f =~ /${templ}\.pot$/) { $files{$1}{pot} = $f; }
+  elsif($f =~ /^${templ}-/) { warn "Skip $f"; }
   else { die "unknown file name $f."; }
 }
 mkdir "upl";
