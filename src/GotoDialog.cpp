73dedf0ff3075fafe4172e79e28518780c848a0b
Chris "Koying" Browet <cbro@semperpax.com>
2010-03-22 00:51:47 +0000

    ADD : allow google maps links in goto and download dialogs
    git-svn-id: http://svn.openstreetmap.org/applications/editors/merkaartor@20598 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- src/GotoDialog.cpp
+++ src/GotoDialog.cpp
@@ -18,168 +18,168 @@
 #include "Utils/OsmLink.h"
 
 GotoDialog::GotoDialog(const MapView& aView, QWidget *parent)
-	:QDialog(parent)
+    :QDialog(parent)
 {
-	setupUi(this);
+    setupUi(this);
 
-	setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
-	setWindowFlags(windowFlags() | Qt::MSWindowsFixedSizeDialogHint);
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    setWindowFlags(windowFlags() | Qt::MSWindowsFixedSizeDialogHint);
 
-	CoordBox B = aView.viewport();
-	int OsmZoom = int((log((360.0 / intToAng(B.latDiff()))) / log(2.0)) + 1);
-	OsmZoom = qMin(OsmZoom, 18);
-	OsmZoom = qMax(OsmZoom, 1);
+    CoordBox B = aView.viewport();
+    int OsmZoom = int((log((360.0 / intToAng(B.latDiff()))) / log(2.0)) + 1);
+    OsmZoom = qMin(OsmZoom, 18);
+    OsmZoom = qMax(OsmZoom, 1);
 
-	int idx = 0;
-	int selIdx = -1;
-	double dist = 6372.795;
-	double d;
-	BookmarkList* Bookmarks = M_PREFS->getBookmarks();
-	BookmarkListIterator i(*Bookmarks);
-	while (i.hasNext()) {
-		i.next();
+    int idx = 0;
+    int selIdx = -1;
+    double dist = 6372.795;
+    double d;
+    BookmarkList* Bookmarks = M_PREFS->getBookmarks();
+    BookmarkListIterator i(*Bookmarks);
+    while (i.hasNext()) {
+        i.next();
 
-		if (i.value().deleted == false) {
-			coordBookmark->addItem(i.key());
-			CoordBox C = i.value().Coordinates;
-			if ((d = C.center().distanceFrom(B.center())) < dist) {
-				dist = d;
-				selIdx = idx;
-			}
-		}
-		++idx;
-	}
-	coordBookmark->setCurrentIndex(selIdx);
+        if (i.value().deleted == false) {
+            coordBookmark->addItem(i.key());
+            CoordBox C = i.value().Coordinates;
+            if ((d = C.center().distanceFrom(B.center())) < dist) {
+                dist = d;
+                selIdx = idx;
+            }
+        }
+        ++idx;
+    }
+    coordBookmark->setCurrentIndex(selIdx);
 
-	searchWidget = new NameFinder::NameFinderWidget(this);
-	connect(searchWidget, SIGNAL(selectionChanged()), this, SLOT(searchWidget_selectionChanged()));
-	connect(searchWidget, SIGNAL(doubleClicked()), this, SLOT(searchWidget_doubleClicked()));
-	connect(searchWidget, SIGNAL(done()), this, SLOT(searchWidget_done()));
-	verticalLayout_4->addWidget(searchWidget);
+    searchWidget = new NameFinder::NameFinderWidget(this);
+    connect(searchWidget, SIGNAL(selectionChanged()), this, SLOT(searchWidget_selectionChanged()));
+    connect(searchWidget, SIGNAL(doubleClicked()), this, SLOT(searchWidget_doubleClicked()));
+    connect(searchWidget, SIGNAL(done()), this, SLOT(searchWidget_done()));
+    verticalLayout_4->addWidget(searchWidget);
 
-	coordOSM->setText( QString("http://www.openstreetmap.org/?lat=%1&lon=%2&zoom=%3")
-		.arg(QString::number(intToAng(B.center().lat()), 'f', 4))
-		.arg(QString::number(intToAng(B.center().lon()), 'f', 4))
-		.arg(QString::number(OsmZoom))
-		);
-	coordOsmApi->setText( QString("http://www.openstreetmap.org/api/%1/map?bbox=%2,%3,%4,%5")
-		.arg(M_PREFS->apiVersion())
-		.arg(QString::number(intToAng(B.bottomLeft().lon()), 'f', 4))
-		.arg(QString::number(intToAng(B.bottomLeft().lat()), 'f', 4))
-		.arg(QString::number(intToAng(B.topRight().lon()), 'f', 4))
-		.arg(QString::number(intToAng(B.topRight().lat()), 'f', 4))
-		);
-	coordOsmXApi->setText( QString("http://xapi.openstreetmap.org/api/0.5/*[bbox=%1,%2,%3,%4]")
-		.arg(QString::number(intToAng(B.bottomLeft().lon()), 'f', 4))
-		.arg(QString::number(intToAng(B.bottomLeft().lat()), 'f', 4))
-		.arg(QString::number(intToAng(B.topRight().lon()), 'f', 4))
-		.arg(QString::number(intToAng(B.topRight().lat()), 'f', 4))
-		);
-	coordCoord->setText( QString("%1, %2, %3, %4")
-		.arg(QString::number(intToAng(B.bottomLeft().lon()), 'f', 4))
-		.arg(QString::number(intToAng(B.bottomLeft().lat()), 'f', 4))
-		.arg(QString::number(intToAng(B.topRight().lon()), 'f', 4))
-		.arg(QString::number(intToAng(B.topRight().lat()), 'f', 4))
-		);
-	coordSpan->setText( QString("%1, %2, %3, %4")
-		.arg(QString::number(intToAng(B.center().lat()), 'f', 4))
-		.arg(QString::number(intToAng(B.center().lon()), 'f', 4))
-		.arg(QString::number(intToAng(B.latDiff()), 'f', 4))
-		.arg(QString::number(intToAng(B.lonDiff()), 'f', 4))
-		);
+    coordLink->setText( QString("http://www.openstreetmap.org/?lat=%1&lon=%2&zoom=%3")
+        .arg(QString::number(intToAng(B.center().lat()), 'f', 4))
+        .arg(QString::number(intToAng(B.center().lon()), 'f', 4))
+        .arg(QString::number(OsmZoom))
+        );
+    coordOsmApi->setText( QString("http://www.openstreetmap.org/api/%1/map?bbox=%2,%3,%4,%5")
+        .arg(M_PREFS->apiVersion())
+        .arg(QString::number(intToAng(B.bottomLeft().lon()), 'f', 4))
+        .arg(QString::number(intToAng(B.bottomLeft().lat()), 'f', 4))
+        .arg(QString::number(intToAng(B.topRight().lon()), 'f', 4))
+        .arg(QString::number(intToAng(B.topRight().lat()), 'f', 4))
+        );
+    coordOsmXApi->setText( QString("http://xapi.openstreetmap.org/api/0.5/*[bbox=%1,%2,%3,%4]")
+        .arg(QString::number(intToAng(B.bottomLeft().lon()), 'f', 4))
+        .arg(QString::number(intToAng(B.bottomLeft().lat()), 'f', 4))
+        .arg(QString::number(intToAng(B.topRight().lon()), 'f', 4))
+        .arg(QString::number(intToAng(B.topRight().lat()), 'f', 4))
+        );
+    coordCoord->setText( QString("%1, %2, %3, %4")
+        .arg(QString::number(intToAng(B.bottomLeft().lon()), 'f', 4))
+        .arg(QString::number(intToAng(B.bottomLeft().lat()), 'f', 4))
+        .arg(QString::number(intToAng(B.topRight().lon()), 'f', 4))
+        .arg(QString::number(intToAng(B.topRight().lat()), 'f', 4))
+        );
+    coordSpan->setText( QString("%1, %2, %3, %4")
+        .arg(QString::number(intToAng(B.center().lat()), 'f', 4))
+        .arg(QString::number(intToAng(B.center().lon()), 'f', 4))
+        .arg(QString::number(intToAng(B.latDiff()), 'f', 4))
+        .arg(QString::number(intToAng(B.lonDiff()), 'f', 4))
+        );
 
-	resize(1,1);
+    resize(1,1);
 }
 
 void GotoDialog::on_searchButton_clicked()
 {
-		QString searchString = NameFinderEdit->text();
-		searchWidget->search(searchString);
-		searchButton->setEnabled(false);
+        QString searchString = NameFinderEdit->text();
+        searchWidget->search(searchString);
+        searchButton->setEnabled(false);
 }
 void GotoDialog::on_buttonBox_clicked(QAbstractButton * button)
 {
-	if (buttonBox->buttonRole(button) == QDialogButtonBox::AcceptRole) {
-		if (rbBookmark->isChecked()) {
-			theNewViewport = M_PREFS->getBookmarks()->value(coordBookmark->currentText()).Coordinates;
-		} else
-		if (rbOSM->isChecked()) {
-			OsmLink ol(coordOSM->text());
-			if (!ol.isValid()) {
-				QMessageBox::warning(this, QApplication::translate("GotoDialog", "Invalid OSM url"),
-					QApplication::translate("GotoDialog", "The specified url is invalid!"));
-				return;
-			}
-			theNewViewport = ol.getCoordBox();
-		} else
-		if (rbCoord->isChecked()) {
-			QStringList tokens = coordCoord->text().split(",");
-			if (tokens.size() < 4) {
-				QMessageBox::warning(this, QApplication::translate("GotoDialog", "Invalid Coordinates format"),
-					QApplication::translate("GotoDialog", "Coordinates must be: '<left lon>, <bottom lat>, <right lon>, <top lat>'"));
-				return;
-			}
-			theNewViewport = CoordBox(Coord(angToInt(tokens[1].toDouble()), angToInt(tokens[0].toDouble())), Coord(angToInt(tokens[3].toDouble()), angToInt(tokens[2].toDouble())));
-		} else
-		if (rbSpan->isChecked()) {
-			QStringList tokens = coordSpan->text().split(",");
-			if (tokens.size() < 4) {
-				QMessageBox::warning(this, QApplication::translate("GotoDialog", "Invalid Coordinates format"),
-					QApplication::translate("GotoDialog", "Coordinates must be: '<center lat>, <center lon>, <span lat>, <span lon>'"));
-				return;
-			}
-			theNewViewport = CoordBox(
-								Coord(
-									angToInt(tokens[0].toDouble() - tokens[2].toDouble() / 2),
-									angToInt(tokens[1].toDouble() - tokens[3].toDouble() / 2)),
-								Coord(
-									angToInt(tokens[0].toDouble() + tokens[2].toDouble() / 2),
-									angToInt(tokens[1].toDouble() + tokens[3].toDouble() / 2))
-								);
-		}
-		accept();
-	}
+    if (buttonBox->buttonRole(button) == QDialogButtonBox::AcceptRole) {
+        if (rbBookmark->isChecked()) {
+            theNewViewport = M_PREFS->getBookmarks()->value(coordBookmark->currentText()).Coordinates;
+        } else
+        if (rbLink->isChecked()) {
+            OsmLink ol(coordLink->text());
+            if (!ol.isValid()) {
+                QMessageBox::warning(this, QApplication::translate("GotoDialog", "Invalid OSM url"),
+                    QApplication::translate("GotoDialog", "The specified url is invalid!"));
+                return;
+            }
+            theNewViewport = ol.getCoordBox();
+        } else
+        if (rbCoord->isChecked()) {
+            QStringList tokens = coordCoord->text().split(",");
+            if (tokens.size() < 4) {
+                QMessageBox::warning(this, QApplication::translate("GotoDialog", "Invalid Coordinates format"),
+                    QApplication::translate("GotoDialog", "Coordinates must be: '<left lon>, <bottom lat>, <right lon>, <top lat>'"));
+                return;
+            }
+            theNewViewport = CoordBox(Coord(angToInt(tokens[1].toDouble()), angToInt(tokens[0].toDouble())), Coord(angToInt(tokens[3].toDouble()), angToInt(tokens[2].toDouble())));
+        } else
+        if (rbSpan->isChecked()) {
+            QStringList tokens = coordSpan->text().split(",");
+            if (tokens.size() < 4) {
+                QMessageBox::warning(this, QApplication::translate("GotoDialog", "Invalid Coordinates format"),
+                    QApplication::translate("GotoDialog", "Coordinates must be: '<center lat>, <center lon>, <span lat>, <span lon>'"));
+                return;
+            }
+            theNewViewport = CoordBox(
+                                Coord(
+                                    angToInt(tokens[0].toDouble() - tokens[2].toDouble() / 2),
+                                    angToInt(tokens[1].toDouble() - tokens[3].toDouble() / 2)),
+                                Coord(
+                                    angToInt(tokens[0].toDouble() + tokens[2].toDouble() / 2),
+                                    angToInt(tokens[1].toDouble() + tokens[3].toDouble() / 2))
+                                );
+        }
+        accept();
+    }
 }
 
 void GotoDialog::searchWidget_selectionChanged()
 {
-	QPointF centerPoint = searchWidget->selectedCoords();
-	int zoom = searchWidget->selectedZoom();
-	// The API doesn't like request for too large bounding boxes so reset to a default
-	if (zoom < 15)
-		zoom = 15;
-	coordOSM->setText( QString("http://www.openstreetmap.org/?lat=%1&lon=%2&zoom=%3")
-		.arg(QString::number(centerPoint.x(), 'f', 4))
-		.arg(QString::number(centerPoint.y(), 'f', 4))
-				.arg(QString::number(zoom))
-		);
-	rbOSM->setChecked(true);
+    QPointF centerPoint = searchWidget->selectedCoords();
+    int zoom = searchWidget->selectedZoom();
+    // The API doesn't like request for too large bounding boxes so reset to a default
+    if (zoom < 15)
+        zoom = 15;
+    coordLink->setText( QString("http://www.openstreetmap.org/?lat=%1&lon=%2&zoom=%3")
+        .arg(QString::number(centerPoint.x(), 'f', 4))
+        .arg(QString::number(centerPoint.y(), 'f', 4))
+                .arg(QString::number(zoom))
+        );
+    rbLink->setChecked(true);
 
 }
 
 void GotoDialog::on_NameFinderEdit_textChanged(const QString & text)
 {
-	if (!text.isEmpty()) {
-		searchButton->setDefault(true);
-	} else {
-		searchButton->setDefault(false);
-		buttonBox->button(QDialogButtonBox::Ok)->setDefault(true);
-	}
+    if (!text.isEmpty()) {
+        searchButton->setDefault(true);
+    } else {
+        searchButton->setDefault(false);
+        buttonBox->button(QDialogButtonBox::Ok)->setDefault(true);
+    }
 }
 
 void GotoDialog::searchWidget_doubleClicked()
 {
-	buttonBox->button(QDialogButtonBox::Ok)->click();
+    buttonBox->button(QDialogButtonBox::Ok)->click();
 }
 
 void GotoDialog::searchWidget_done()
 {
-	searchButton->setEnabled(true);
+    searchButton->setEnabled(true);
 }
 
 void GotoDialog::changeEvent(QEvent * event)
 {
-		if (event->type() == QEvent::LanguageChange)
-				retranslateUi(this);
+        if (event->type() == QEvent::LanguageChange)
+                retranslateUi(this);
 }
 
--- src/Utils/OsmLink.h
+++ src/Utils/OsmLink.h
@@ -8,26 +8,29 @@
 class OsmLink
 {
 public:
-	OsmLink(QUrl url);
-	OsmLink(QString url);
+    OsmLink(QUrl url);
+    OsmLink(QString url);
 
-	bool isValid() const {return m_IsValid;}
-	bool isShort() const {return m_IsShort;}
-	CoordBox getCoordBox();
+    bool isValid() const {return m_IsValid;}
+    bool isShort() const {return m_IsShort;}
+    CoordBox getCoordBox();
 
-	static bool isValid(QUrl theUrl);
+    static bool isValid(QUrl theUrl);
 
 private:
-	QUrl theUrl;
-	bool m_IsValid;
-	bool m_IsShort;
+    QUrl theUrl;
+    bool m_IsValid;
+    bool m_IsShort;
+    bool m_hasSpan;
 
-	double m_Lat;
-	double m_Lon;
-	int m_Zoom;
+    double m_Lat;
+    double m_Lon;
+    double m_spanLat;
+    double m_spanLon;
+    int m_Zoom;
 
 protected:
-	void checkUrl();
+    void checkUrl();
 };
 
 #endif // OSMLINK_H
