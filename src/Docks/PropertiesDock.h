d3ed60cbae05780bbd8ea2a9651d3709930a38c8
Chris "Koying" Browet <cbro@semperpax.com>
2010-03-25 15:53:09 +0000

    ADD : allow to re-order relation members (closes #2759)
    git-svn-id: http://svn.openstreetmap.org/applications/editors/merkaartor@20671 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- src/Docks/PropertiesDock.h
+++ src/Docks/PropertiesDock.h
@@ -21,101 +21,103 @@ class CommandList;
 
 class PropertiesDock : public MDockAncestor
 {
-	Q_OBJECT
+    Q_OBJECT
 
-	public:
-		PropertiesDock(MainWindow* aParent);
-	public:
-		~PropertiesDock(void);
+    public:
+        PropertiesDock(MainWindow* aParent);
+    public:
+        ~PropertiesDock(void);
 
-		void setSelection(Feature* aFeature);
-		void setMultiSelection(Feature* aFeature);
-		template<class T>
-				void setSelection(const QList<T*>& aFeatureList);
-		void setMultiSelection(const QList<Feature*>& aFeatureList);
-		void toggleSelection(Feature* aFeature);
-		void addSelection(Feature* aFeature);
-		void adjustSelection();
-		Feature* selection(int idx);
-		QList<Feature*> selection();
-		bool isSelected(Feature *aFeature);
-		int size() const;
-		void resetValues();
-		void checkMenuStatus();
-		bool loadTemplates(const QString& filename = "");
-		bool mergeTemplates(const QString& filename = "");
-		bool saveTemplates(const QString& filename);
+        void setSelection(Feature* aFeature);
+        void setMultiSelection(Feature* aFeature);
+        template<class T>
+                void setSelection(const QList<T*>& aFeatureList);
+        void setMultiSelection(const QList<Feature*>& aFeatureList);
+        void toggleSelection(Feature* aFeature);
+        void addSelection(Feature* aFeature);
+        void adjustSelection();
+        Feature* selection(int idx);
+        QList<Feature*> selection();
+        bool isSelected(Feature *aFeature);
+        int size() const;
+        void resetValues();
+        void checkMenuStatus();
+        bool loadTemplates(const QString& filename = "");
+        bool mergeTemplates(const QString& filename = "");
+        bool saveTemplates(const QString& filename);
 
-		Feature* highlighted(int idx);
-		QList<Feature*> highlighted();
-		int highlightedSize() const;
+        Feature* highlighted(int idx);
+        QList<Feature*> highlighted();
+        int highlightedSize() const;
 
 
-	public slots:
-		void on_TrackPointLat_editingFinished();
-		void on_TrackPointLon_editingFinished();
-		void on_Member_customContextMenuRequested(const QPoint & pos);
-		void on_Member_clicked(const QModelIndex & index);
-		void on_Member_selected();
-		void on_RemoveMemberButton_clicked();
-		void on_RemoveTagButton_clicked();
-		void on_SelectionList_itemSelectionChanged();
-		void on_SelectionList_itemDoubleClicked(QListWidgetItem* item);
-		void executePendingSelectionChange();
-		void on_SelectionList_customContextMenuRequested(const QPoint & pos);
-		void on_centerAction_triggered();
-		void on_centerZoomAction_triggered();
-		void on_tag_changed(QString k, QString v);
-		void on_tag_cleared(QString k);
-		void on_template_changed(TagTemplate* aNewTemplate);
+    public slots:
+        void on_TrackPointLat_editingFinished();
+        void on_TrackPointLon_editingFinished();
+        void on_Member_customContextMenuRequested(const QPoint & pos);
+        void on_Member_clicked(const QModelIndex & index);
+        void on_Member_selected();
+        void on_btMemberUp_clicked();
+        void on_btMemberDown_clicked();
+        void on_RemoveMemberButton_clicked();
+        void on_RemoveTagButton_clicked();
+        void on_SelectionList_itemSelectionChanged();
+        void on_SelectionList_itemDoubleClicked(QListWidgetItem* item);
+        void executePendingSelectionChange();
+        void on_SelectionList_customContextMenuRequested(const QPoint & pos);
+        void on_centerAction_triggered();
+        void on_centerZoomAction_triggered();
+        void on_tag_changed(QString k, QString v);
+        void on_tag_cleared(QString k);
+        void on_template_changed(TagTemplate* aNewTemplate);
 
-	private:
-		void cleanUpUi();
-		void switchUi();
-		void switchToNoUi();
-		void switchToTrackPointUi();
-		void switchToRoadUi();
-		void switchToMultiUi();
-		void switchToRelationUi();
-		void fillMultiUiSelectionBox();
-		void changeEvent(QEvent*);
-		void retranslateUi();
+    private:
+        void cleanUpUi();
+        void switchUi();
+        void switchToNoUi();
+        void switchToTrackPointUi();
+        void switchToRoadUi();
+        void switchToMultiUi();
+        void switchToRelationUi();
+        void fillMultiUiSelectionBox();
+        void changeEvent(QEvent*);
+        void retranslateUi();
 
-		MainWindow* Main;
-		QWidget* CurrentUi;
-		QList<Feature*> Selection;
-		QList<Feature*> FullSelection;
-		Ui::TrackPointProperties TrackPointUi;
-		Ui::RoadProperties RoadUi;
-		Ui::MultiProperties MultiUi;
-		Ui::RelationProperties RelationUi;
-		TagModel* theModel;
-		int PendingSelectionChange;
-		EditCompleterDelegate* delegate;
-		QAction* centerAction;
-		QAction* centerZoomAction;
-		QAction* selectAction;
-		ShortcutOverrideFilter* shortcutFilter;
-		TagTemplates* theTemplates;
+        MainWindow* Main;
+        QWidget* CurrentUi;
+        QList<Feature*> Selection;
+        QList<Feature*> FullSelection;
+        Ui::TrackPointProperties TrackPointUi;
+        Ui::RoadProperties RoadUi;
+        Ui::MultiProperties MultiUi;
+        Ui::RelationProperties RelationUi;
+        TagModel* theModel;
+        int PendingSelectionChange;
+        EditCompleterDelegate* delegate;
+        QAction* centerAction;
+        QAction* centerZoomAction;
+        QAction* selectAction;
+        ShortcutOverrideFilter* shortcutFilter;
+        TagTemplates* theTemplates;
 
-		QList<Feature*> Highlighted;
+        QList<Feature*> Highlighted;
 
-		QTableView *CurrentTagView;
-		QTableView *CurrentMembersView;
+        QTableView *CurrentTagView;
+        QTableView *CurrentMembersView;
 
-		enum { NoUiShowing, TrackPointUiShowing, RoadUiShowing, RelationUiShowing, MultiShowing } NowShowing ;
+        enum { NoUiShowing, TrackPointUiShowing, RoadUiShowing, RelationUiShowing, MultiShowing } NowShowing ;
 };
 
 template<class T>
-		void PropertiesDock::setSelection(const QList<T*>& aFeatureList)
+        void PropertiesDock::setSelection(const QList<T*>& aFeatureList)
 {
-	cleanUpUi();
-	Selection.clear();
-	for (int i=0; i<aFeatureList.size(); ++i)
-		Selection.push_back(aFeatureList[i]);
-	FullSelection = Selection;
-	switchUi();
-	fillMultiUiSelectionBox();
+    cleanUpUi();
+    Selection.clear();
+    for (int i=0; i<aFeatureList.size(); ++i)
+        Selection.push_back(aFeatureList[i]);
+    FullSelection = Selection;
+    switchUi();
+    fillMultiUiSelectionBox();
 }
 
 
