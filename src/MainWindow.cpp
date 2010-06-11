0efb962d15b14396f5d334e6e99313a2b5314401
Chris "koying" Browet <cbro@semperpax.com>
2010-06-11 17:14:35 +0200

    FIX : Force "C" locale for double

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -95,6 +95,8 @@
 #include <QMenu>
 #include <QTcpServer>
 
+#include <locale.h>
+
 SlippyMapCache* SlippyMapWidget::theSlippyCache = 0;
 
 class MainWindowPrivate
@@ -127,6 +129,8 @@ MainWindow::MainWindow(QWidget *parent)
         , qtTranslator(0)
         , merkaartorTranslator(0)
 {
+    setlocale(LC_NUMERIC, "C");  // impose decimal point separator
+
     p = new MainWindowPrivate;
 
     theProgressDialog = NULL;
