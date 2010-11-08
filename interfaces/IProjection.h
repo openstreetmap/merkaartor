a869a24b6a55afb1f4e32d019a5b7072d79601ff
Chris "Koying" Browet <cbro@semperpax.com>
2010-11-09 00:54:17 +0100

    forgot file

---
+++ interfaces/IProjection.h
@@ -0,0 +1,21 @@
+#ifndef MERKATOR_IPROJECTION_H_
+#define MERKATOR_IPROJECTION_H_
+
+class QString;
+class QPointF;
+
+class IProjection
+{
+public:
+    virtual ~IProjection(void) {};
+
+    virtual QPointF project(const QPointF& pt) const = 0;
+    virtual QPointF inverse2Point(const QPointF& pt) const = 0;
+
+    virtual QString getProjectionType() const = 0;
+};
+
+
+#endif
+
+
