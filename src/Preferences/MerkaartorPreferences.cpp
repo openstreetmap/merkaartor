b6ae432d317024cbee5c2b2dd313997994066262
Chris "Koying" Browet <cbro@semperpax.com>
2011-01-19 00:38:25 +0100

    FIX : do not save locale info to registry in portable mode

--- src/Preferences/MerkaartorPreferences.cpp
+++ src/Preferences/MerkaartorPreferences.cpp
@@ -1707,8 +1707,13 @@ void MerkaartorPreferences::saveOsmServers()
 QString getDefaultLanguage(bool returnDefault)
 {
     if (!g_Merk_Ignore_Preferences && !g_Merk_Reset_Preferences) {
-        QSettings Sets;
-        QString lang = Sets.value("locale/language").toString();
+        QSettings* Sets;
+        if (!g_Merk_Portable) {
+            Sets = new QSettings();
+        } else {
+            Sets = new QSettings(qApp->applicationDirPath() + "/merkaartor.ini", QSettings::IniFormat);
+        }
+        QString lang = Sets->value("locale/language").toString();
         if (lang == "")
             if (returnDefault)
                 lang = QLocale::system().name().split("_")[0];
@@ -1724,7 +1729,12 @@ QString getDefaultLanguage(bool returnDefault)
 void setDefaultLanguage(const QString& theValue)
 {
     if (!g_Merk_Ignore_Preferences) {
-        QSettings Sets;
-        Sets.setValue("locale/language", theValue);
+        QSettings* Sets;
+        if (!g_Merk_Portable) {
+            Sets = new QSettings();
+        } else {
+            Sets = new QSettings(qApp->applicationDirPath() + "/merkaartor.ini", QSettings::IniFormat);
+        }
+        Sets->setValue("locale/language", theValue);
     }
 }
