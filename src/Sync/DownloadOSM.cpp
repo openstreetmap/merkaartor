openstreetmap/merkaartor

commit 33118d2c61161c82f519b93456b205fb09fee433

FIX: Replaced QHttp in DownloadOSM.
This is the last QHttp use.

src/Sync/DownloadOSM.cpp
@@ -38,13 +38,11 @@
 
 Downloader::Downloader(const QString& aUser, const QString& aPwd)
 : User(aUser), Password(aPwd),
-  Id(0),Error(false), AnimatorLabel(0), AnimatorBar(0), AnimationTimer(0)
+  currentReply(0),Error(false), AnimatorLabel(0), AnimatorBar(0), AnimationTimer(0)
 {
     //IdAuth = Request.setUser(User.toUtf8(), Password.toUtf8());
-//	connect(&Request,SIGNAL(done(bool)), this,SLOT(allDone(bool)));
-    connect(&Request,SIGNAL(requestFinished(int, bool)),this,SLOT(on_requestFinished(int, bool)));
-    connect(&Request,SIGNAL(dataReadProgress(int, int)), this,SLOT(progress(int, int)));
-    connect(&Request, SIGNAL(responseHeaderReceived(const QHttpResponseHeader &)), this, SLOT(on_responseHeaderReceived(const QHttpResponseHeader &)));
+    connect(&netManager,SIGNAL(finished(QNetworkReply*)),this,SLOT(on_requestFinished(QNetworkReply*)));
+    connect(&netManager,SIGNAL(authenticationRequired(QNetworkReply*,QAuthenticator*)), this,SLOT(on_authenticationRequired(QNetworkReply*,QAuthenticator*)));
 }
 
 
@@ -82,7 +80,7 @@ void Downloader::on_Cancel_clicked()
         Loop.exit(QDialog::Rejected);
 }
 
-#include "QtGui/QTextBrowser"
+#include "QTextBrowser"
 
 void showDebug(const QString& Method, const QString& URL, const QString& Sent, const QByteArray& arr)
 {
@@ -172,122 +170,98 @@ bool Downloader::go(const QUrl& url)
 {
     if (Error) return false;
 
-    Request.setProxy(M_PREFS->getProxy(url));
-    Request.setHost(url.host(),url.port(80));
+    netManager.setProxy(M_PREFS->getProxy(url));
+
+    qDebug() << "Downloader::go: " << url;
+
+    QNetworkRequest req(url);
 
     if (AnimationTimer)
         AnimationTimer->start(200);
-    Content.clear();
-    QBuffer ResponseBuffer(&Content);
-    ResponseBuffer.open(QIODevice::WriteOnly);
-    QString sReq = url.toString(QUrl::RemoveScheme | QUrl::RemoveAuthority);
-    qDebug() << "Downloader::go: " << url << sReq;
-    QHttpRequestHeader Header("GET",sReq);
-    Header.setValue("Accept-Encoding", "gzip,deflate");
-    Header.setValue("Host",url.host()+':'+QString::number(url.port(80)));
-    Header.setValue("User-Agent", USER_AGENT);
-    Content.clear();
-    Id = Request.request(Header,QByteArray(), &ResponseBuffer);
+
+    req.setRawHeader(QByteArray("Accept-Encoding"), QByteArray("gzip,deflate"));
+    req.setRawHeader(QByteArray("User-Agent"), USER_AGENT.toLatin1());
+
+    currentReply = netManager.get(req);
+    connect(currentReply,SIGNAL(downloadProgress(qint64, qint64)), this,SLOT(progress(qint64, qint64)));
 
     if (Loop.exec() == QDialog::Rejected)
-    {
-        Request.abort();
         return false;
+
+    if (currentReply->error())
+        QMessageBox::information(0,tr("error"), currentReply->errorString());
+
+    // FIXME: Is is worth it? I don't think the API even specifies length in advance.
+    //if (Request.lastResponse().hasContentLength() && Content.size() != (int)Request.lastResponse().contentLength())
+    //   QMessageBox::information(0,tr("didn't download enough"),QString("%1 %2").arg(Content.size()).arg(Request.lastResponse().contentLength()));
+
+    QByteArray encoding = currentReply->rawHeader(QByteArray("Content-encoding"));
+    if (encoding == "gzip") {
+        Content = gzipDecode(currentReply->readAll());
+    } else {
+        qDebug() << "Downloader: received non-compressed content. The encoding was: " << encoding;
+        Content = currentReply->readAll();
     }
 
-    if (Error)
-    {
-        QMessageBox::information(0,tr("error"),Request.errorString());
-    }
-    if (Request.lastResponse().hasContentLength() && Content.size() != (int)Request.lastResponse().contentLength())
-    {
-        QMessageBox::information(0,tr("didn't download enough"),QString("%1 %2").arg(Content.size()).arg(Request.lastResponse().contentLength()));
-    }
-
-    if (Request.lastResponse().hasKey("Content-encoding"))
-    {
-        QString t(Request.lastResponse().value("Content-encoding"));
-        if (t == "gzip")
-        {
-            QByteArray Uncompressed(gzipDecode(Content));
-            Content = Uncompressed;
-        }
-    }
-
-#ifdef DEBUG_EVERY_CALL
-    showDebug("GET", Web.path() + url, QByteArray() ,Content);
-#endif
     SAFE_DELETE(AnimationTimer);
-    LocationText = Request.lastResponse().value("Location");
 
-    Result = Request.lastResponse().statusCode();
-    switch (Result) {
-    case 301:
-    case 302:
-    case 307: {
-            qDebug() << "New location: " << LocationText;
-            if (!LocationText.isEmpty()) {
-                QUrl aURL(LocationText);
-                return go(aURL);
-            }
-            break;
+    QVariant redir = currentReply->attribute(QNetworkRequest::RedirectionTargetAttribute);
+    if (redir.isValid()) {
+        LocationText = redir.toString();
+        if (!LocationText.isEmpty()) {
+            QUrl aURL(LocationText);
+            return go(aURL);
         }
     }
 
-    ResultText = Request.lastResponse().reasonPhrase();
-    ErrorText = Request.lastResponse().value("Error");
+    Result = currentReply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+    ResultText = currentReply->errorString();
+    ErrorText = currentReply->rawHeader("Error");
+
     return !Error;
 }
 
-bool Downloader::request(const QString& Method, const QUrl& url, const QString& Data, bool FireForget)
+bool Downloader::request(const QString& theMethod, const QUrl& url, const QString& theData, bool FireForget)
 {
     if (Error) return false;
 
-    Request.setProxy(M_PREFS->getProxy(url));
-    Request.setHost(url.host(),url.port(80));
+    netManager.setProxy(M_PREFS->getProxy(url));
+
     qDebug() << "Downloader::request: " << url;
 
-    QByteArray ba(Data.toUtf8());
-    QBuffer Buf(&ba);
+    QNetworkRequest req(url);
 
-    QString sReq = url.toString(QUrl::RemoveScheme | QUrl::RemoveAuthority);
-    QHttpRequestHeader Header(Method,sReq);
-    Header.setValue("Host",url.host()+':'+QString::number(url.port(80)));
-    Header.setValue("User-Agent", USER_AGENT);
-    Header.setValue("Content-Type", "text/xml");
+    req.setRawHeader(QByteArray("Content-Type"), QByteArray("text/xml"));
+    req.setRawHeader(QByteArray("User-Agent"), USER_AGENT.toLatin1());
 
-    QString auth = QString("%1:%2").arg(User).arg(Password);
-    QByteArray ba_auth = auth.toUtf8().toBase64();
-    Header.setValue("Authorization", QString("Basic %1").arg(QString(ba_auth)));
-
-    Content.clear();
-    Id = Request.request(Header,ba);
+    QByteArray dataArray(theData.toUtf8());
+    QBuffer dataBuffer(&dataArray);
+    currentReply = netManager.sendCustomRequest(req, theMethod.toLatin1(), &dataBuffer);
 
     if (FireForget)
         return true;
 
     if (Loop.exec() == QDialog::Rejected)
-    {
-        Request.abort();
         return false;
+
+    QByteArray encoding = currentReply->rawHeader(QByteArray("Content-encoding"));
+    if (encoding == "gzip") {
+        Content = gzipDecode(currentReply->readAll());
+    } else {
+        qDebug() << "Downloader: received non-compressed content. The encoding was: " << encoding;
+        Content = currentReply->readAll();
     }
-    Content = Request.readAll();
-    if (Request.lastResponse().hasKey("Content-encoding"))
-    {
-        QString t(Request.lastResponse().value("Content-encoding"));
-        if (t == "gzip")
-        {
-            QByteArray Uncompressed(gzipDecode(Content));
-            Content = Uncompressed;
-        }
-    }
-#ifdef DEBUG_NONGET_CALL
-    showDebug(Method,URL,Data,Content);
-#endif
-    Result = Request.lastResponse().statusCode();
-    ResultText = Request.lastResponse().reasonPhrase();
-    ErrorText = Request.lastResponse().value("Error");
+
+    Result = currentReply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+    ResultText = currentReply->errorString();
+    ErrorText = currentReply->rawHeader("Error");
     return !Error;
+}
+
+void Downloader::on_authenticationRequired( QNetworkReply *reply, QAuthenticator *auth) {
+    Q_UNUSED(reply);
+    auth->setUser(User);
+    auth->setPassword(Password);
 }
 
 QByteArray& Downloader::content()
@@ -295,6 +269,7 @@ QByteArray& Downloader::content()
     return Content;
 }
 
+/*
 void Downloader::on_responseHeaderReceived(const QHttpResponseHeader & hdr)
 {
     //switch (hdr.statusCode()) {
@@ -313,17 +288,17 @@ void Downloader::on_responseHeaderReceived(const QHttpResponseHeader & hdr)
     //}
 
     qDebug() << "Downloader::on_responseHeaderReceived: " << hdr.statusCode() << hdr.reasonPhrase();
-}
+} */
 
-void Downloader::on_requestFinished(int anId, bool anError)
+void Downloader::on_requestFinished( QNetworkReply *reply)
 {
-    if (anError)
+    if (reply->error())
         Error = true;
-    if ( (anId == Id) && Loop.isRunning() )
+    if ( (reply == currentReply) && Loop.isRunning() )
         Loop.exit(QDialog::Accepted);
 }
 
-void Downloader::progress(int done, int total)
+void Downloader::progress(qint64 done, qint64 total)
 {
     if (AnimatorLabel && AnimatorBar)
     {
@@ -790,6 +765,7 @@ bool downloadMoreOSM(MainWindow* Main, const CoordBox& aBox , Document* theDocum
     osmWebsite = M_PREFS->getOsmApiUrl();
     osmUser = M_PREFS->getOsmUser();
     osmPwd = M_PREFS->getOsmPassword();
+    qDebug() << "Requesting more from " << osmWebsite;
 
     Main->view()->setUpdatesEnabled(false);
 

src/Sync/DownloadOSM.h
@@ -3,7 +3,6 @@
 
 class Document;
 
-class QHttp;
 class QString;
 class QMainWindow;
 class QProgressBar;
@@ -19,7 +18,7 @@ class SpecialLayer;
 #include <QtCore/QByteArray>
 #include <QtCore/QEventLoop>
 #include <QtCore/QObject>
-#include <QtNetwork/QHttp>
+#include <QNetworkAccessManager>
 #include <QUrl>
 
 #include "IFeature.h"
@@ -31,7 +30,7 @@ class Downloader : public QObject
     public:
         Downloader(const QString& aUser, const QString& aPwd);
 
-        bool request(const QString& Method, const QUrl& URL, const QString& Out, bool FireForget=false);
+        bool request(const QString& theMethod, const QUrl& URL, const QString& Out, bool FireForget=false);
         bool go(const QUrl& url);
         QByteArray& content();
         int resultCode();
@@ -53,21 +52,21 @@ class Downloader : public QObject
         void setAnimator(QProgressDialog *anAnimator, QLabel* AnimatorLabel, QProgressBar* AnimatorBar, bool anAnimate);
 
     public slots:
-        void progress( int done, int total );
-        void on_requestFinished(int Id, bool Error);
-        void on_responseHeaderReceived(const QHttpResponseHeader & hdr);
+        void progress( qint64 done, qint64 total );
+        void on_requestFinished( QNetworkReply *reply);
+        void on_authenticationRequired( QNetworkReply *reply, QAuthenticator *auth);
         void animate();
         void on_Cancel_clicked();
 
     private:
-        QHttp Request;
+        QNetworkAccessManager netManager;
         QString User, Password;
         QByteArray Content;
         int Result;
         QString LocationText;
         QString ResultText;
         QString ErrorText;
-        int Id;
+        QNetworkReply *currentReply;
         int IdAuth;
         bool Error;
         QEventLoop Loop;

commit 75def704efee7696f1df043759a99d30f5753f93

FIX: Update after upload.
The update code was wrong, as it tried to decode gzip itself. QNetwork*
already does this for us, provided you don't provide the Accept-Encoding
manually (as it happened in ::go).

This fixed bug introduced in 33118d2.

src/Sync/DownloadOSM.cpp
@@ -28,8 +28,6 @@
 #include <QStatusBar>
 #include <QInputDialog>
 
-#include <zlib.h>
-
 // #define DEBUG_EVERY_CALL
 // #define DEBUG_MAPCALL_ONLY
 // #define DEBUG_NONGET_CALL
@@ -100,72 +98,6 @@ void showDebug(const QString& Method, const QString& URL, const QString& Sent, c
     b->raise();
 }
 
-
-#define CHUNK 4096
-
-QByteArray gzipDecode(const QByteArray& In)
-{
-    QByteArray Total;
-
-    int ret;
-    unsigned have;
-    z_stream strm;
-    char in[CHUNK+2];
-    char out[CHUNK+2];
-    /* allocate inflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.avail_in = 0;
-    strm.next_in = Z_NULL;
-    ret = inflateInit2(&strm,15+32);
-    if (ret != Z_OK)
-       {
-               (void)inflateEnd(&strm);
-        return Total;
-       }
-    int RealSize = In.size();
-    for (int i=0; i<RealSize/CHUNK+1; ++i)
-    {
-        int Left = RealSize-(i*CHUNK);
-        if (Left > CHUNK)
-            Left = CHUNK;
-        memcpy(in,In.constData()+(i*CHUNK),Left);
-        strm.avail_in = Left;
-        strm.next_in = reinterpret_cast<unsigned char*>(in);
-
-        /* run inflate() on input until output buffer not full */
-        do
-        {
-            strm.avail_out = CHUNK;
-            strm.next_out = reinterpret_cast<unsigned char*>(out);
-            ret = inflate(&strm, Z_NO_FLUSH);
-            if (ret == Z_STREAM_ERROR)
-                       {
-                               (void)inflateEnd(&strm);
-                return Total;
-                       }
-            switch (ret)
-            {
-                case Z_NEED_DICT:
-                    ret = Z_DATA_ERROR;     /* and fall through */
-                case Z_DATA_ERROR:
-                case Z_MEM_ERROR:
-                    (void)inflateEnd(&strm);
-                    return Total;
-            }
-            have = CHUNK - strm.avail_out;
-            out[have] = 0;
-            Total.append(QByteArray(out,have));
-        } while (strm.avail_out == 0);
-    }
-       (void)inflateEnd(&strm);
-    return Total;
-}
-
-
-
-
 bool Downloader::go(const QUrl& url)
 {
     if (Error) return false;
@@ -179,7 +111,6 @@ bool Downloader::go(const QUrl& url)
     if (AnimationTimer)
         AnimationTimer->start(200);
 
-    req.setRawHeader(QByteArray("Accept-Encoding"), QByteArray("gzip,deflate"));
     req.setRawHeader(QByteArray("User-Agent"), USER_AGENT.toLatin1());
 
     currentReply = netManager.get(req);
@@ -196,12 +127,7 @@ bool Downloader::go(const QUrl& url)
     //   QMessageBox::information(0,tr("didn't download enough"),QString("%1 %2").arg(Content.size()).arg(Request.lastResponse().contentLength()));
 
     QByteArray encoding = currentReply->rawHeader(QByteArray("Content-encoding"));
-    if (encoding == "gzip") {
-        Content = gzipDecode(currentReply->readAll());
-    } else {
-        qDebug() << "Downloader: received non-compressed content. The encoding was: " << encoding;
-        Content = currentReply->readAll();
-    }
+    Content = currentReply->readAll();
 
     SAFE_DELETE(AnimationTimer);
 
@@ -244,13 +170,11 @@ bool Downloader::request(const QString& theMethod, const QUrl& url, const QStrin
     if (Loop.exec() == QDialog::Rejected)
         return false;
 
+    if (currentReply->error())
+        QMessageBox::information(0,tr("error"), currentReply->errorString());
+
     QByteArray encoding = currentReply->rawHeader(QByteArray("Content-encoding"));
-    if (encoding == "gzip") {
-        Content = gzipDecode(currentReply->readAll());
-    } else {
-        qDebug() << "Downloader: received non-compressed content. The encoding was: " << encoding;
-        Content = currentReply->readAll();
-    }
+    Content = currentReply->readAll();
 
     Result = currentReply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
     ResultText = currentReply->errorString();
@@ -268,27 +192,6 @@ QByteArray& Downloader::content()
 {
     return Content;
 }
-
-/*
-void Downloader::on_responseHeaderReceived(const QHttpResponseHeader & hdr)
-{
-    //switch (hdr.statusCode()) {
-    //	case 200:
-    //		break;
-    //	case 406:
-    //		QMessageBox::critical(NULL,QApplication::translate("MerkaartorPreferences","Preferences upload failed"), QApplication::translate("MerkaartorPreferences","Duplicate key"));
-    //		break;
-    //	case 413:
-    //		QMessageBox::critical(NULL,QApplication::translate("MerkaartorPreferences","Preferences upload failed"), QApplication::translate("MerkaartorPreferences","More than 150 preferences"));
-    //		break;
-    //	default:
-    //		qDebug() << hdr.statusCode();
-    //		qDebug() << hdr.reasonPhrase();
-    //		break;
-    //}
-
-    qDebug() << "Downloader::on_responseHeaderReceived: " << hdr.statusCode() << hdr.reasonPhrase();
-} */
 
 void Downloader::on_requestFinished( QNetworkReply *reply)
 {