b6f4f7435f784a113265e9d682f4a19070dfaf6b
Ladislav Laska <krakonos@krakonos.org>
2016-05-28 17:44:03 +0200

    Added translations to installed files.

--- windows/copydeps.sh
+++ windows/copydeps.sh
@@ -6,3 +6,8 @@ cp `cygcheck.exe binaries/bin/merkaartor.exe  | grep mingw.*dll$ | sed -e 's/^ *
 # Copy qwindows.dll, as it's loaded dynamically at runtime and not discovered by cygcheck
 mkdir binaries/bin/platforms
 cp /mingw*/share/qt5/plugins/platforms/qwindows.dll binaries/bin/platforms/
+
+# Copy translations
+mkdir binaries/bin/translations
+cp translations/*.qm binaries/bin/translations/
+cp /mingw*/share/qt5/translations/q*.qm binaries/bin/translations/
