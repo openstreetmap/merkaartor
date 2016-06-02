633e43e4a00efb0353084029ef68473ae6cd0ce9
Ladislav Laska <krakonos@krakonos.org>
2016-06-02 11:46:09 +0200

    Fix windows deployment to include all plugins.

--- windows/copydeps.sh
+++ windows/copydeps.sh
@@ -3,11 +3,15 @@
 # Copy Merkaartor and it's binary dependencies
 cp `cygcheck.exe binaries/bin/merkaartor.exe  | grep mingw.*dll$ | sed -e 's/^ *//' | sort -u` ./binaries/bin/
 
-# Copy qwindows.dll, as it's loaded dynamically at runtime and not discovered by cygcheck
-mkdir binaries/bin/platforms
-cp /mingw*/share/qt5/plugins/platforms/qwindows.dll binaries/bin/platforms/
 
-# Copy translations
+# Run Windeployqt, mostly to copy all the plugins required by qt, and some more libs
+# The ICU hack is there due to a bug in windeployqt, that won't find the lib
+# with "lib" prefix. We later delete it not to have it twice.
+ICU=/mingw64/bin/libicudt[0-9][0-9].dll
+cp "$ICU" "${ICU/libicudt/icudt}"
+windeployqt binaries/bin/merkaartor.exe
+rm binaries/bin/icudt[0-9][0-9].dll
+
+# Copy translations, qt ones are already copied by windeployqt
 mkdir binaries/bin/translations
 cp translations/*.qm binaries/bin/translations/
-cp /mingw*/share/qt5/translations/q*.qm binaries/bin/translations/
