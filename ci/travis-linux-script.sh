4c7a48783c1e6a5a2c6a185db329a4c6eb6c0cc8
probonopd <probonopd@users.noreply.github.com>
2018-12-10 21:47:15 +0000

    Update travis-linux-script.sh

--- ci/travis-linux-script.sh
+++ ci/travis-linux-script.sh
@@ -12,7 +12,7 @@ wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/contin
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
-( cd src ; ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage)
+( cd src ; ../linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage)
 find src/appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 # curl --upload-file src/Merkaartor*.AppImage https://transfer.sh/Merkaartor-git.$(git rev-parse --short HEAD)-x86_64.AppImage
 wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
