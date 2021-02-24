d31c617be14a87930bce896a2bbb65ee255c56b0
Ladislav Láska <krakonos@krakonos.org>
2021-02-24 11:31:43 +0100

    Removed extra uninstall steps from travis windows script.

--- ci/travis-windows-install.ps1
+++ ci/travis-windows-install.ps1
@@ -1,13 +1,4 @@
-
-powershell Set-MpPreference -DisableRealTimeMonitoring 1
-powershell Set-MpPreference -DisableBehaviorMonitoring 1
-
-gdr
-
-choco list -localonly
-choco uninstall -y mingw llvm wsl
-
-gdr
+tgdr
 
 choco install -q msys2
 taskkill -IM "gpg-agent.exe" -F
@@ -16,6 +7,11 @@ gdr
 
 c:\tools\msys64\usr\bin\bash.exe -l -c "pacman --noconfirm --needed -S msys/make msys/git mingw64/mingw-w64-x86_64-gcc mingw64/mingw-w64-x86_64-qt5 mingw64/mingw-w64-x86_64-gdal mingw64/mingw-w64-x86_64-proj mingw64/mingw-w64-x86_64-openjpeg2 mingw64/mingw-w64-x86_64-json-c mingw64/mingw-w64-x86_64-cmake mingw64/mingw-w64-x86_64-exiv2 mingw64/mingw-w64-x86_64-nsis"
 
+# At some point, we had issues with disk space on travis windows. Here are a few tricks that helped:
+## Uninstall some choco packages we are not using
+#
+# choco uninstall -y mingw llvm wsl
+#
 ## Install Qt first, as it requires quite a bit of space
 #c:\tools\msys64\usr\bin\bash.exe -l -c "pacman --noconfirm --needed -S mingw64/mingw-w64-x86_64-qt5"
 #
