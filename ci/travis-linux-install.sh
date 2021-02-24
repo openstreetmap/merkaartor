ad680949d3c2721461e83bddbc65aa22cad13bd0
Ladislav Láska <krakonos@krakonos.org>
2021-02-24 10:06:49 +0100

    Removed unsupported ubuntu variants trusty and bionic.
    They have too old PROJ package - we require 6.x+, but they are stuck at
    4.x.

--- ci/travis-linux-install.sh
+++ ci/travis-linux-install.sh
@@ -5,9 +5,6 @@ set -ev
 env
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1397BC53640DB551
 sudo echo "deb http://archive.ubuntu.com/ubuntu ${TRAVIS_DIST} main universe restricted multiverse" \> /etc/apt/sources.list
-if [ "${TRAVIS_DIST}" == trusty ]; then
-	sudo apt-add-repository -y ppa:ubuntu-sdk-team/ppa
-fi
 sudo apt-add-repository -y ${QT_REPO}
 sudo apt-get update -qq
 sudo apt-get -y install gdb libgdal-dev libproj-dev qt${QT_PREFIX}base qt${QT_PREFIX}tools qt${QT_PREFIX}svg build-essential libgl1-mesa-dev cmake git libexiv2-dev
