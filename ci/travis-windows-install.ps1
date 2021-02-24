d097fa60d28e19f2cd9f7e6002a9fa409e4b82b4
Ladislav Láska <krakonos@krakonos.org>
2021-02-24 11:05:53 +0100

    Removed base-devel package group to install faster on windows CI.
    It may need some additional packages though.

--- ci/travis-windows-install.ps1
+++ ci/travis-windows-install.ps1
@@ -14,7 +14,7 @@ taskkill -IM "gpg-agent.exe" -F
 
 gdr
 
-c:\tools\msys64\usr\bin\bash.exe -l -c "pacman --noconfirm --needed -S base-devel msys/git mingw64/mingw-w64-x86_64-gcc mingw64/mingw-w64-x86_64-qt5 mingw64/mingw-w64-x86_64-gdal mingw64/mingw-w64-x86_64-proj mingw64/mingw-w64-x86_64-openjpeg2 mingw64/mingw-w64-x86_64-json-c mingw64/mingw-w64-x86_64-cmake mingw64/mingw-w64-x86_64-exiv2 mingw64/mingw-w64-x86_64-nsis"
+c:\tools\msys64\usr\bin\bash.exe -l -c "pacman --noconfirm --needed -S msys/make msys/git mingw64/mingw-w64-x86_64-gcc mingw64/mingw-w64-x86_64-qt5 mingw64/mingw-w64-x86_64-gdal mingw64/mingw-w64-x86_64-proj mingw64/mingw-w64-x86_64-openjpeg2 mingw64/mingw-w64-x86_64-json-c mingw64/mingw-w64-x86_64-cmake mingw64/mingw-w64-x86_64-exiv2 mingw64/mingw-w64-x86_64-nsis"
 
 ## Install Qt first, as it requires quite a bit of space
 #c:\tools\msys64\usr\bin\bash.exe -l -c "pacman --noconfirm --needed -S mingw64/mingw-w64-x86_64-qt5"
