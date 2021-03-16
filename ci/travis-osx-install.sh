e726b863474fe0db7625fe7f37e3298a2a753cc2
Ladislav Láska <krakonos@krakonos.org>
2021-03-16 20:53:25 +0100

    Updated OSX scripts to use CMake tooling.

--- ci/travis-osx-install.sh
+++ ci/travis-osx-install.sh
@@ -4,17 +4,11 @@ set -ev
 
 brew update
 
-if [ $QT == 4 ]; then
-    QT_PKG=qt
-else
-    #QT_PKG=homebrew/versions/qt52
-    QT_PKG=qt5
-fi
 
 # Uninstall ALL the GDAL. By default, there are more versions and macdeployqt
 # tried to embed an older one, that relies upon libspatialite.5, that is no
 # longer installed.
-brew uninstall --ignore-dependencies --force gdal
+brew uninstall --ignore-dependencies --force gdal exiv2 proj qt5
 
 # To get GDAL 2.0: http://gis.stackexchange.com/questions/155403/install-gdal-2-0-on-a-macosx
 #brew install gdal --HEAD
@@ -24,8 +18,5 @@ brew uninstall --ignore-dependencies --force gdal
 #
 # Moreover, we will ignore the results as homebrew fails as it wishes.  The
 # build will fail later anyway if something doesn't install properly.
-brew install Dylibbundler gdal $QT_PKG || echo "Install might have failed. Ignoring"
-brew link --force $QT_PKG || echo "Link might have failed. Ignoring."
-
-find /usr/local -name "qmake*"
-find /usr/local -name "proj_api.h"
+brew install Dylibbundler proj exiv2 gdal qt5 || echo "Install might have failed. Ignoring"
+brew link --force qt5 || echo "Link might have failed. Ignoring."
--- ci/travis-osx-script.sh
+++ ci/travis-osx-script.sh
@@ -2,27 +2,12 @@
 
 set -ev
 
-git tag
-QMAKE=`find /usr/local -name "qmake" | head -n 1`
-$QMAKE SPATIALITE=0
-make -j3
-lrelease src/src.pro
-
-find ./binaries
-
-cd binaries/bin
-mv plugins merkaartor.app/Contents/
-
-which hdiutil
-
-DEPLOY=`find /usr/local -name "macdeployqt" | head -n 1`
-$DEPLOY merkaartor.app -verbose=3
-
-du -s -h merkaartor.app
-hdiutil create "merkaartor.dmg" -srcfolder merkaartor.app -format UDZO -volname merkaartor -verbose -size 200m
+mkdir build && cd build
+cmake .. -G"Unix Makefiles"
+make -j4
+make VERBOSE=1 package
 
 VERSION=`git describe --tags`
-mv merkaartor.dmg "merkaartor-${VERSION}-qt${QT}.dmg"
 
 # Prepare deployment description
 cat > deploy.json <<EOF
@@ -39,7 +24,7 @@ cat > deploy.json <<EOF
 
     "files":
         [
-        {"includePattern": "binaries/bin/(merkaartor-.*.dmg)", "uploadPattern":"\$1"}
+        {"includePattern": "build/(merkaartor-.*.dmg)", "uploadPattern":"\$1"}
         ],
     "publish": true
 }
