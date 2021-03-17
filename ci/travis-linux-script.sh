0890bcf44b5db19346e79a06f6b94f6c05a3280e
Ladislav Láska <krakonos@krakonos.org>
2021-03-17 15:38:12 +0100

    Fixed linux script to use the system cmake, instead of old travis-provided one.

--- ci/travis-linux-script.sh
+++ ci/travis-linux-script.sh
@@ -5,6 +5,7 @@ source /opt/qt${QT_PREFIX}/bin/qt${QT_PREFIX}-env.sh
 set -ev
 
 mkdir build && cd build
-cmake ..
+# Note: We need to specify the system cmake, as travis has older version in PATH before the system paths for some reason.
+/usr/bin/cmake ..
 make -j3
 QT_QPA_PLATFORM=offscreen ctest --verbose
