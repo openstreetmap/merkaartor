49ff885928dfe79f2e705f51cb09eae8f56e5a69
Ladislav Láska <krakonos@krakonos.org>
2021-03-17 14:59:21 +0100

    Debug info into linux build.

--- ci/travis-linux-install.sh
+++ ci/travis-linux-install.sh
@@ -9,4 +9,7 @@ sudo echo "deb http://archive.ubuntu.com/ubuntu ${TRAVIS_DIST} main universe res
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
 sudo apt-add-repository -y ${QT_REPO}
 sudo apt-get update -qq
+which cmake
 sudo apt-get -y install gdb libgdal-dev libproj-dev qt${QT_PREFIX}base qt${QT_PREFIX}tools qt${QT_PREFIX}svg build-essential libgl1-mesa-dev cmake git libexiv2-dev
+which cmake
+dpkg-query -L cmake
