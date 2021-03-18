1adfcb2d2fa2bd2cbf7779b289579358cff03fd4
Ladislav Láska <krakonos@krakonos.org>
2021-03-18 16:08:08 +0100

    Moved deploy.json to CMake, added tests to all platforms.

--- ci/travis-osx-script.sh
+++ ci/travis-osx-script.sh
@@ -4,29 +4,6 @@ set -ev
 
 mkdir build && cd build
 cmake .. -G"Unix Makefiles" -DCMAKE_PREFIX_PATH="`brew --prefix qt5`"
-make VEROBSE=1 -j4
-make VERBOSE=1 package
-
-VERSION=`git describe --tags`
-
-# Prepare deployment description
-cat > deploy.json <<EOF
-{
-    "package": {
-        "name": "Merkaartor",
-        "repo": "nightly",
-        "subject": "krakonos"
-    },
-
-    "version": {
-        "name": "$VERSION"
-    },
-
-    "files":
-        [
-        {"includePattern": "build/(merkaartor-.*.dmg)", "uploadPattern":"\$1"},
-        {"includePattern": "build/(merkaartor-.*.zip)", "uploadPattern":"\$1"}
-        ],
-    "publish": true
-}
-EOF
+make -j4
+make test
+make package
--- ci/travis-windows-script.sh
+++ ci/travis-windows-script.sh
@@ -5,33 +5,5 @@ set -e
 mkdir build && cd build
 cmake .. -G"Unix Makefiles"
 make -j4
-make VERBOSE=1 package
-
-#TODO: lrelease src/src.pro
-
-#TODO: sh windows/copydeps.sh
-#TODO: makensis.exe windows/installer.nsi
-#windows/upload-to-bintray.pl windows/merkaartor-*.exe
-
-# Prepare deployment description
-VERSION=`git describe --tags --dirty`
-cat > deploy.json <<EOF
-{
-    "package": {
-        "name": "Merkaartor",
-        "repo": "nightly",
-        "subject": "krakonos"
-    },
-
-    "version": {
-        "name": "$VERSION"
-    },
-
-    "files":
-        [
-        {"includePattern": "build/merkaartor-*.exe", "uploadPattern":"\$1"},
-        {"includePattern": "build/merkaartor-*.zip", "uploadPattern":"\$1"}
-        ],
-    "publish": true
-}
-EOF
+make test
+make package
