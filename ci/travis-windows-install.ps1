dae7cc25de62632eab754d01074bc80c0bea8b0e
Ladislav Láska <krakonos@krakonos.org>
2021-03-19 08:38:25 +0100

    Added mesa to windows install to hopefully add missing opengl32.dll.

--- ci/travis-windows-install.ps1
+++ ci/travis-windows-install.ps1
@@ -5,7 +5,8 @@ taskkill -IM "gpg-agent.exe" -F
 
 gdr
 
-c:\tools\msys64\usr\bin\bash.exe -l -c "pacman --noconfirm --needed -S msys/make msys/git mingw64/mingw-w64-x86_64-gcc mingw64/mingw-w64-x86_64-qt5 mingw64/mingw-w64-x86_64-gdal mingw64/mingw-w64-x86_64-proj mingw64/mingw-w64-x86_64-openjpeg2 mingw64/mingw-w64-x86_64-json-c mingw64/mingw-w64-x86_64-cmake mingw64/mingw-w64-x86_64-exiv2 mingw64/mingw-w64-x86_64-nsis"
+# We also install mingw-w64-x86_64-mesa, a per https://travis-ci.community/t/opengl32-dll-is-missing/10472/6 , it should provide the missing opengl32.dll for our tests to run.
+c:\tools\msys64\usr\bin\bash.exe -l -c "pacman --noconfirm --needed -S msys/make msys/git mingw64/mingw-w64-x86_64-gcc mingw64/mingw-w64-x86_64-qt5 mingw64/mingw-w64-x86_64-gdal mingw64/mingw-w64-x86_64-proj mingw64/mingw-w64-x86_64-openjpeg2 mingw64/mingw-w64-x86_64-json-c mingw64/mingw-w64-x86_64-cmake mingw64/mingw-w64-x86_64-exiv2 mingw64/mingw-w64-x86_64-nsis mingw64/mingw-w64-x86_64-mesa"
 
 # At some point, we had issues with disk space on travis windows. Here are a few tricks that helped:
 ## Uninstall some choco packages we are not using
