5b785d4b841ddfe80b8635f07675662ab41145e5
Ladislav Láska <krakonos@krakonos.org>
2021-03-20 23:09:36 +0100

    Updated cmake to pickup version from latest tag.

--- cmake/vcs.cmake
+++ cmake/vcs.cmake
@@ -4,40 +4,47 @@ if (NOT ${GIT_FOUND})
     message(FATAL_ERROR "Git not found. Cannot proceed with version info.")
 endif()
 
-if (NOT EXISTS "${PROJECT_SOURCE_DIR}/.git")
+if (NOT EXISTS "${CMAKE_SOURCE_DIR}/.git")
     message(FATAL_ERROR "VCS directory does not contain a .git subdirectory. Not a repository?")
 endif()
 
 # Set dependencies on git index to reconfigure on changed index/branch (like switching branches, commiting, etc.).
-set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${PROJECT_SOURCE_DIR}/.git/index")
-set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${PROJECT_SOURCE_DIR}/.git/HEAD")
+set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${CMAKE_SOURCE_DIR}/.git/index")
+set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${CMAKE_SOURCE_DIR}/.git/HEAD")
 
 execute_process(
     COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     OUTPUT_VARIABLE  VCS_BRANCH
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 execute_process(
     COMMAND ${GIT_EXECUTABLE} rev-parse --short=12 HEAD
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     OUTPUT_VARIABLE  VCS_COMMIT
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 execute_process(
     COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     OUTPUT_VARIABLE  VCS_DESCRIBE
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 execute_process(
     COMMAND ${GIT_EXECUTABLE} diff-index --quiet HEAD --
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     RESULT_VARIABLE  VCS_DIRTY
 )
+
+# Describe has format 0.18.4-137-gXXX, or 0.18.4 if exactly on tag. We normalize that to:
+# 0.18.4 -> 0.18.4
+# 0.18.4-137-gXXX -> 0.18.4.137
+# 0.18.4-rcX-137-gXXX -> 0.18.4.137
+# And use it as version that can be parsed by the cmake VERSION
+string(REGEX REPLACE "(-rc[0-9]+)?-([0-9]*)-g[0-9a-z]*(-dirty)?" ".\\2" VCS_VERSION ${VCS_DESCRIBE})
 
 # Workaround to update dirty flag when anything changes, as CMake can not
 # depend on a command that would check it the git way. This check is only used
@@ -50,5 +57,5 @@ execute_process(
 #    properly
 # This workaround shall be replaced if a better way is found.
 if (NOT ${VCS_DIRTY})
-    file(GLOB_RECURSE DEP_FILES CONFIGURE_DEPENDS "${PROJECT_SOURCE_DIR}/*.cpp" "${PROJECT_SOURCE_DIR}/*.h")
+    file(GLOB_RECURSE DEP_FILES CONFIGURE_DEPENDS "${CMAKE_SOURCE_DIR}/*.cpp" "${CMAKE_SOURCE_DIR}/*.h")
 endif()
