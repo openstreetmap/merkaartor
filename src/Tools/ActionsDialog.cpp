66eac3347e4817cc341cedb803cc2e8d0404117f
Ladislav Láska <krakonos@krakonos.org>
2022-07-14 20:13:21 +0200

    Relaced remaining uses of setMargins with setContentsMargins.

--- src/Tools/ActionsDialog.cpp
+++ src/Tools/ActionsDialog.cpp
@@ -69,7 +69,7 @@ ActionsDialog::ActionsDialog(QList<QAction *>& actions, MainWindow *parent)
     buttonLayout->addWidget(cancelButton);
 
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    mainLayout->setMargin(8);
+    mainLayout->setContentsMargins(8, 8, 8, 8);
     mainLayout->setSpacing(8);
     mainLayout->addWidget(actionsTable);
     mainLayout->addLayout(buttonLayout);
