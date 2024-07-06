7edb6f8171dcc28cf4e1ea665c4fe4ab6f8e8e16
Ladislav Láska <krakonos@krakonos.org>
2024-07-06 21:12:44 +0200

    Replaced qt5 wildcard with specific version.

--- ci/travis-linux-install.sh
+++ ci/travis-linux-install.sh
@@ -9,4 +9,4 @@ sudo echo "deb http://archive.ubuntu.com/ubuntu ${TRAVIS_DIST} main universe res
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
 sudo apt-add-repository -y ${QT_REPO}
 sudo apt-get update -qq
-sudo apt-get -y install gdb libgdal-dev libproj-dev qt${QT_PREFIX}base qt${QT_PREFIX}tools qt${QT_PREFIX}svg qt${QT_PREFIX}networkauth* build-essential libgl1-mesa-dev cmake git libexiv2-dev protobuf-compiler
+sudo apt-get -y install gdb libgdal-dev libproj-dev qt${QT_PREFIX}base qt${QT_PREFIX}tools qt${QT_PREFIX}svg qt${QT_PREFIX}networkauth-no-lgpl build-essential libgl1-mesa-dev cmake git libexiv2-dev protobuf-compiler
