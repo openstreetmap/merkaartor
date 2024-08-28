df19d746fe8dbc385e92840275d3781cc5835e08
Ladislav Láska <krakonos@krakonos.org>
2024-08-28 15:19:10 +0200

    Added dbus manually on osx.

--- ci/travis-osx-script.sh
+++ ci/travis-osx-script.sh
@@ -4,6 +4,7 @@ set -ev
 
 mkdir build && cd build
 QT_PREFIX=`brew --prefix qt${QT_MAJOR}`
+find /usr/local -name "QtD*.framework"
 cmake .. -G"Unix Makefiles" -DCMAKE_PREFIX_PATH="$QT_PREFIX" -DCMAKE_BUILD_TYPE=Release -DEXTRA_TESTS=OFF
 make -j4
 make test
