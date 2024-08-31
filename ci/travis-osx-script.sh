87154dc0aa106aa24696bc2a9c75c88e23f3e4ad
Ladislav Láska <krakonos@krakonos.org>
2024-08-31 16:38:33 +0200

    Replaced homebrew with conda.

--- ci/travis-osx-script.sh
+++ ci/travis-osx-script.sh
@@ -3,10 +3,10 @@
 set -ev
 
 mkdir build && cd build
-QT_PREFIX=`brew --prefix qt${QT_MAJOR}`
-echo "Using QT_PREFIX=$QT_PREFIX"
-find /usr/local -name "QtD*.framework"
-cmake .. -G"Unix Makefiles" -DCMAKE_PREFIX_PATH="$QT_PREFIX" -DCMAKE_BUILD_TYPE=Release -DEXTRA_TESTS=OFF
+conda info
+echo $PATH
+ls /Users/runner/miniconda3/condabin
+cmake .. -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DEXTRA_TESTS=OFF
 make -j4
 make test
 for i in {0..10}; do
