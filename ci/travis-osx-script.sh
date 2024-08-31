df4960b537b94464d42dea4b96c081cfefaf8671
Ladislav Láska <krakonos@krakonos.org>
2024-08-31 15:57:16 +0200

    Add retry attempts to package creation in osx due to race condition on 3rd party tooling.

--- ci/travis-osx-script.sh
+++ ci/travis-osx-script.sh
@@ -4,8 +4,16 @@ set -ev
 
 mkdir build && cd build
 QT_PREFIX=`brew --prefix qt${QT_MAJOR}`
+echo "Using QT_PREFIX=$QT_PREFIX"
 find /usr/local -name "QtD*.framework"
 cmake .. -G"Unix Makefiles" -DCMAKE_PREFIX_PATH="$QT_PREFIX" -DCMAKE_BUILD_TYPE=Release -DEXTRA_TESTS=OFF
 make -j4
 make test
-make package
+for i in {0..10}; do
+    # make package seems to fail due to race conditions:
+    # https://github.com/actions/runner-images/issues/7522
+    # https://gitlab.kitware.com/cmake/cmake/-/issues/25671
+    make package && break
+    echo "Failed attempt #${i}. Retrying..."
+done
+echo "Passed on attempt #${i}."
