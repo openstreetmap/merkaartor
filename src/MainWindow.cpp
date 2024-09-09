aba3f2e70e7b84d1edafe0f83b4a0ac665658bd2
Ladislav Láska <krakonos@krakonos.org>
2024-09-09 19:10:00 +0200

    Allow word wrap on viewport status bar.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -228,8 +228,11 @@ MainWindow::MainWindow(QWidget *parent)
     MeterPerPixelLabel = new QLabel(this);
     AdjusmentMeterLabel = new QLabel(this);
 
+    ViewportStatusLabel->setWordWrap(true);
+    ViewportStatusLabel->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+
     pbImages = new QProgressBar(this);
-    statusBar()->addPermanentWidget(ViewportStatusLabel);
+    statusBar()->addPermanentWidget(ViewportStatusLabel, true);
     statusBar()->addPermanentWidget(pbImages);
     statusBar()->addPermanentWidget(MeterPerPixelLabel);
     statusBar()->addPermanentWidget(AdjusmentMeterLabel);
